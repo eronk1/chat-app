708b14376068c7ec8842e743ee2b34b072b8471e
eronk1 <jeongahkim8174@gmail.com>
2024-03-01 14:17:22 -0800

    made some more direct message ui changes

--- chatAppFrontend/src/chatPages/MessageScreen/MessageScreenChatParts/MessageScreenChatPartsParent.jsx
+++ chatAppFrontend/src/chatPages/MessageScreen/MessageScreenChatParts/MessageScreenChatPartsParent.jsx
@@ -5,13 +5,18 @@ import './MessageScreenChatPartsParent.css'
 export default function MessageScreenChatPartsParent() {
   let messages = [{sender:'@me', order:1, message:"blah blah blah blah something i ate something then something uwu something something"}]
   return (
-    <div>
+    <div id='the-actual-fr-message-parent'>
       <ActualMessage sender="@me" message={"blah blah blah asfkdjkladfsjklfd sajklfasd klkfjladsjlk dfaslj fasdklkjlfadskjladfsj kjlfadskjlafsjd kljklasdfjkafsdkjj asklfdlkjfadsljk flsadkjkjlf daskadfs kllfkadskdafslkj fl adskj"} />
+      <ActualMessage sender="@me" message={"blah blah blah asfkdjkladfsjklfd sajklfasd klkfjladsjlk dfaslj fasdklkjlfadskjladfsj kjlfadskjlafsjd kljklasdfjkafsdkjj asklfdlkjfadsljk flsadkjkjlf daskadfs kllfkadskdafslkj fl adskj"} />
+      <ActualMessage sender="@me" message={"blah blah blah asfkdjkladfsjklfd sajklfasd klkfjladsjlk dfaslj fasdklkjlfadskjladfsj kjlfadskjlafsjd kljklasdfjkafsdkjj asklfdlkjfadsljk flsadkjkjlf daskadfs kllfkadskdafslkj fl adskj"} />
+      <ActualMessage pfp="/cags2.png" sender="anton pro" message={"blah blah blah asfkdjkla fadsjf jkklad flkj fdkjf djkkf dsfdjfs dljkj fk4jije jirf jir ejio erji erjio erji jeiogj erjie joer joijo eriij egiojg e oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjeio egijogejioegijeoj gj ge"} />
+      <ActualMessage pfp="/cags2.png" sender="anton pro" message={"blah blah blah asfkdjkla fadsjf jkklad flkj fdkjf djkkf dsfdjfs dljkj fk4jije jirf jir ejio erji erjio erji jeiogj erjie joer joijo eriij egiojg e oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjeio egijogejioegijeoj gj ge"} />
+      <ActualMessage pfp="/cags2.png" sender="anton pro" message={"blah blah blah asfkdjkla fadsjf jkklad flkj fdkjf djkkf dsfdjfs dljkj fk4jije jirf jir ejio erji erjio erji jeiogj erjie joer joijo eriij egiojg e oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjee oijegjioeg jiogjeio egijogejioegijeoj gj ge"} />
     </div>
   )
 }
 
-function ActualMessage({sender, message}){
+function ActualMessage({sender, message, pfp}){
   if(sender==='@me'){
     return(
       <div className='actual-message-user-message-sent'>
@@ -25,13 +30,19 @@ function ActualMessage({sender, message}){
     )
   }else{
     return(
-      <div className='actual-message-user-message-sent'>
-        <div className='the-actual-message-parent-place'>
-          <div className='the-message-body'>
-            {message}
+      <div id='the-actual-fr-message-parent-fr'>
+        <img className='very-pro-image-lol' src={pfp} alt="cags2 (uwu) failed" />
+        <div className='the-parent-amongst-parents-into-the-wild'>
+          <p className='very-pro-sender-message'>{sender}</p>
+          <div className='actual-message-user-message-sent-other'>
+            <div className='triangle-right-up-other'></div>
+            <div className='the-actual-message-parent-place-server-other'>
+              <div className='the-message-body'>
+                {message}
+              </div>
+            </div>
           </div>
         </div>
-        <div className='triangle-right-up'></div>
       </div>
     )
 
